1da5d0491ee12996be8d69b029a68a66eb1776bf
cache-tlb <liubin1991@outlook.com>
2016-11-10 16:08:11 +0800

    add images

--- src/OpenGLVersion.h
+++ src/OpenGLVersion.h
@@ -1,9 +1,9 @@
 #ifndef QOPENGLVERSION_H
 #define QOPENGLVERSION_H
 
-#include <QOpenGLFunctions_4_5_Core>
+#include <QOpenGLFunctions_4_2_Core>
 
-typedef QOpenGLFunctions_4_5_Core QOpenGLFunctionsType;
+typedef QOpenGLFunctions_4_2_Core QOpenGLFunctionsType;
 
 #endif // QOPENGLVERSION_H
 
